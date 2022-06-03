7620ebadc3e7aeaeffd8fe9c860dd28a32710017
jirany <jiranyang777@gmail.com>
2022-06-03 11:41:38 -0500

    add charts with dropdown

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './Bar';
+import Pie from './Pie'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Pie />
   </React.StrictMode>
 );
 
